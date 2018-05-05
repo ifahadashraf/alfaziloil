9746ddc0b25b699d24da90af5422e4cc72a4334d
ifahadashraf <bcsf14a535@pucit.edu.pk>
2018-05-06 01:38:54 +0500

    initial commit

---
+++ FazalOil/Controllers/MainController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FazalOil.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Main/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+
+        public ActionResult Stock()
+        {
+            return View();
+        }
+
+        public ActionResult Invoice()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateProduct()
+        {
+            return View();
+        }
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ FazalOil/Scripts/custom.js
@@ -0,0 +1,138 @@
+﻿//***********************GLOBAL VARIABLES***************************//
+
+var BASE_URL = "../../";
+var ADD_PRODUCT = "Data/AddProduct";
+
+//***********************END GLOBAL VARIABLES***********************//
+
+
+function supllier() {
+    var location = document.getElementById('selectLocation').value;
+    if(location === "Depot")
+    {
+        document.getElementById('selectSupplier').disabled = true;
+    }
+    else
+    {
+        document.getElementById('selectSupplier').disabled = false;
+    }
+}
+
+function nextChange() {
+    var currentReading = document.getElementById('txtCurrentReading').value;
+    var expectedKm = document.getElementById('txtExpectedkm').value;
+    var nextChange = document.getElementById('txtNextChange');
+    var result = parseInt(currentReading) + parseInt(expectedKm);
+    nextChange.value = result;
+}
+
+function calculateLitres() {
+    var canSize = document.getElementById('txtCanSize').value;
+    var numberOfCans = document.getElementById('txtNoOfCans').value;
+    var numberOfLitres = document.getElementById('txtNoOfLitres');
+    var result = canSize * numberOfCans;
+    numberOfLitres.value = result;
+}
+
+function categories() {
+    var category = $("#selectCategory option:selected").text();
+    if(category === "Oil")
+    {
+        document.getElementById('txtQuantity').disabled = true;
+        document.getElementById('txtCanSize').disabled = false;
+        document.getElementById('txtNoOfCans').disabled = false;
+    }
+    else
+    {
+        document.getElementById('txtQuantity').disabled = false;
+        document.getElementById('txtCanSize').disabled = true;
+        document.getElementById('txtNoOfCans').disabled = true;
+    }
+}
+    
+function totalPrice() {
+    var category = document.getElementById('selectCategory').value;
+    var canPrice = document.getElementById('txtPrice').value;
+    var numberOfCans = document.getElementById('txtNoOfCans').value;
+    var quantity = document.getElementById('txtQuantity').value;
+    var totalPrice1 = document.getElementById('txtTotalPrice');
+    
+    if(category === "Oil")
+    {
+        var result = canPrice * numberOfCans;
+        totalPrice1.value = result;
+    }
+    else
+    {
+        var result = canPrice * quantity;
+        totalPrice1.value = result;
+    }
+}
+
+function saveProduct() {
+
+    var productName = $("#txtProductName").val();
+    var canSize = $("#txtCanSize").val();
+    var totalCans = $("#txtNoOfCans").val();
+    var totalLiters = $("#txtNoOfLitres").val();
+    var quantity = $("#txtQuantity").val();
+    var brandID = $("#selectBrand").val();
+    var purchasingPrice = $("#txtPrice").val();
+    var dropoffID = $("#selectLocation").val();
+    var supplierID = $("#selectSupplier").val();
+    var dateOfPurchase = $("#datePurchase").val();
+
+    var category = $("#selectCategory").val();
+
+    var json = ""
+
+    if (category == "1") {
+        json = {
+            "ProductName": productName,
+            "CanSize": canSize,
+            "totalCans": totalCans,
+            "totalLiters":totalLiters,
+            "BrandID": brandID,
+            "PurchasingPrice": purchasingPrice,
+            "DropoffID": dropoffID,
+            "SupplierID": supplierID,
+            "DateOfPurchase": dateOfPurchase
+        }
+    }
+    else {
+        json = {
+            "ProductName": productName,
+            "Quantity":quantity,
+            "BrandID": brandID,
+            "PurchasingPrice": purchasingPrice,
+            "DropoffID": dropoffID,
+            "SupplierID": supplierID,
+            "DateOfPurchase": dateOfPurchase
+        }
+    }
+    
+    $("#alertdiv").html('<div class="alert alert-warning"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Please wait...</div>');
+
+
+    $.ajax({
+        url: BASE_URL + ADD_PRODUCT,
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(json),
+        success:function(data){
+            $("#alertdiv").html('<div class="alert alert-success">Successfully added</div>');
+            $('input').val('');
+            $('option').attr('selected', false);
+            setTimeout(
+              function () {
+                  //do something special
+                  $("#alertdiv").html('');
+              }, 3000);
+
+            
+        },
+        error:function(err){
+
+        }
+    });
+}
